dcc170680aa2ac1f00a83f92fc8111dc6ad54b20
Alex James <alexatcaesars@gmail.com>
2023-06-09 19:36:26 -0400

    Edit Function to pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, time) {
+    if (time < 0)  return "error";
+    let string = "";
+    for ( let i=0; i < time; i++) {
+      string += word
+    }
+    return string
+  };
+  console.log(repeatString("hey", 3))
+  
+  
 
 // Do not edit below this line
 module.exports = repeatString;
